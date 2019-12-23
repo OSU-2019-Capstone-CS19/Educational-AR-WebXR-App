991848bec0d022a3ef5b1d50fad0352c2f22b987
asnow4u <asnow4u@gmail.com>
2019-12-23 06:58:17 -0700

    couple bug fixs

--- server.js
+++ server.js
@@ -1,39 +1,15 @@
-// var http = require('http');
-var fs = require("fs");
-
-var THREE = require("./three");
+var http = require('http');
 var express = require("express");
-var path = require("path");
 var app = express();
+var server = http.createServer(app);
 const port = 8080;
 
+app.use('/model', express.static(__dirname + '/model'));
+app.use('/src', express.static(__dirname + '/src'));
+
 app.get('/', function(req, res) {
-
-  innerWidth = 660;
-  innerHeight = 500;
-
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 1, 1000 );
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize( innerWidth, innerHeight);
-  document.body.appendChild( renderer.domElement );
-
-  var sun_geometry = new THREE.SphereGeometry( 4, 32, 32 );
-  var yellowMaterial = new THREE.MeshBasicMaterial({color: 0xffff00});
-  var sphere1 = new THREE.Mesh(sun_geometry, yellowMaterial);
-  scene.add(sphere1);
-
-  camera.position.set(0, 30, 50);
-  camera.lookAt(sphere1.position);
-
-  renderer.render(scene, camera);
-
-  renderer.domElement.toBuffer (err, buf) => {
-    res.contentType 'image/jpg';
-    res.send buf;
-  };
-
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port);
+server.listen(port);
 console.log("Server running on port " + port);

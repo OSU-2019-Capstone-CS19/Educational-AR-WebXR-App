daa47f6cb33c93e60a0c55f49164552b4dc4aead
asnow4u <asnow4u@gmail.com>
2019-12-23 23:06:53 -0700

    Reading the JSON file

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = 8080;
 
 app.use('/model', express.static(__dirname + '/model'));
 app.use('/src', express.static(__dirname + '/src'));
+app.use('/solarSystem.json', express.static(__dirname + '/solarSystem.json'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');

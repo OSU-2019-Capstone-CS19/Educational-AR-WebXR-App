b219a054c4dc14fadde1cdd6eb62fbbf54f0a025
asnow4u <asnow4u@gmail.com>
2019-12-30 10:46:19 -0700

    add orbits to planets

--- src/solar.js
+++ src/solar.js
@@ -13,9 +13,9 @@ var request = new XMLHttpRequest();
 //Camera
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.x = -10
-camera.position.z = 10;
-camera.position.y = 5;
+//camera.position.x = -30
+camera.position.z = 300;
+camera.position.y = 300;
 camera.lookAt(new THREE.Vector3( 0, 0, 0));
 
 //Scene
@@ -29,43 +29,19 @@ for (var i=0; i < jsonObj.numPlanets; i++){ //need to add pluto
 }
 
 //Lights
-var sunLight = new THREE.PointLight( 0xff000, 1, 1000, 1);
+var sunLight = new THREE.PointLight( 0xfffff, 1, 1000, 2);
 sunLight.position.set( 0, 0, 0);
 scene.add(sunLight);
+
+//NOTE: May remove and rely on just the point light from the sun. In this case we do need a light to light up the sun from what the camera sees
 var pointLight = new THREE.DirectionalLight(0xffffff, 1);
-pointLight.position.set(0, 0, 200);
+pointLight.position.set(camera.position.x, camera.position.y, camera.position.z);
 scene.add(pointLight);
 
 //Renderer
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-// renderer.gammaOutput = true;
-// renderer.gammaFactor = 2.2;
-
-var render = () => {
-  requestAnimationFrame( render );
-
-  //rotation
-  // for (var i=0; i<sunObj.children.length(); i++){
-  //   if (sunObj.children[i]){
-  //       sunObj.children[i].rotation.y += 0.02;
-  //   }
-  // }
-
-  // if (mercuryObj){
-  //   mercuryObj.rotation.y += 0.02;
-  // }
-  //
-  //Orbit
-  // for (var i=0; i<5; i++){ //will use jsonObj.numElements
-  //   if (pivots[i]){
-  //     pivots[i].rotation.y += 0.02; //be based on the rotation within jsonObj
-  //   }
-  // }
-
-  renderer.render( scene, camera );
-};
 
 //Load Models
 var loader = new THREE.GLTFLoader();
@@ -73,13 +49,16 @@ var loader = new THREE.GLTFLoader();
 var loadSun = ( gltf ) => {
   sunObj = gltf.scene;
   sunObj.position.set(0, 0, 0);
+  sunObj.scale.set((jsonObj.sun.radius/jsonObj.sizeScale/10), //10 for testing
+                    (jsonObj.sun.radius/jsonObj.sizeScale/10),
+                    (jsonObj.sun.radius/jsonObj.sizeScale/10));
   scene.add(sunObj);
 };
 
 var loadAstronaut = ( gltf ) => {
   astronautObj = gltf.scene;
   astronautObj.position.set(0, 0, 0);
-  astronautObj.scale.set(3, 3, 3);
+  astronautObj.scale.set(0, 0, 0);
   scene.add(astronautObj);
 };
 
@@ -133,5 +112,25 @@ for (var i=0; i < jsonObj.numPlanets; i++){    //need to add pluto
     error => onError(error)
   );
 }
+
+var render = () => {
+  requestAnimationFrame( render );
+
+  //rotation
+  // for (var i=0; i<sunObj.children.length(); i++){
+  //   if (sunObj.children[i]){
+  //       sunObj.children[i].rotation.y += 0.02;
+  //   }
+  // }
+
+  //Orbit
+  for (var i=0; i<jsonObj.numPlanets; i++){ //will use jsonObj.numElements
+    if (pivots[i]){
+      pivots[i].rotation.y += jsonObj.planets[i].Rotation / jsonObj.rotationScale;
+    }
+  }
+
+  renderer.render( scene, camera );
+};
 
 render();

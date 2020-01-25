6d35e968b4c91cb65ca763e84ee34b35f5f9102c
strozzi1 <strozzij@oregonstate.edu>
2020-01-25 03:03:20 -0800

    started on side nav and gui

--- src/menuEventHandler.js
+++ src/menuEventHandler.js
@@ -1,6 +1,8 @@
 //Test Button
+
+
 document.getElementById("testButton").addEventListener("click", function(){
-    for (var i=0; i<jsonObj.numPlanets; i++){
+    /*for (var i=0; i<jsonObj.numPlanets; i++){
       jsonObj.planets[i].beingViewed = "false";
     }
     jsonObj.sun.beingViewed = "false";
@@ -8,7 +10,13 @@ document.getElementById("testButton").addEventListener("click", function(){
 
     pivots[2].add(camera);
     camera.position.set( (jsonObj.planets[2].distanceFromSun/jsonObj.distanceScale) * 9/10 , jsonObj.planets[2].radius/jsonObj.sizeScale, 0);
-
-
-
+    */
+    //display menu
 });
+
+function openNav(){
+  document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}

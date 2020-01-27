caad0556fc3a987cb62ba205fb0dda00818b13ec
strozzi1 <strozzij@oregonstate.edu>
2020-01-26 22:54:15 -0800

    Sliders added, speed is wonky

--- src/menuEventHandler.js
+++ src/menuEventHandler.js
@@ -69,10 +69,10 @@ function showSun(){
   
 }
 
-document.getElementById("intensitySlider").addEventListener("click", function(){
+/*document.getElementById("intensitySlider").addEventListener("click", function(){
   jsonObj.sun.intensity=jsonObj.sun.intensity+1.0;
   console.log("sun intensity = ", jsonObj.sun.intensity);
-});
+});*/
 
 document.getElementById("toggleAstronaut").addEventListener("click", function(){
   jsonObj.astronaut.visible=!(jsonObj.astronaut.visible);
@@ -98,7 +98,50 @@ document.getElementById("toggleTrace").addEventListener("click", function(){
 });
 
 
+// Start of Range slider collapsible content
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+////----- Here's range slider stuff
+
+var sunSlider = document.getElementById("sunRange");
+var output = document.getElementById("sunVal");
+sunSlider.value = jsonObj.sun.intensity;
+output.innerHTML = sunSlider.value;
+
+sunSlider.oninput = function() {
+  output.innerHTML = this.value;
+  jsonObj.sun.intensity=this.value;
+}
+
+var speedSlider = document.getElementById("speedRange");
+var speedOutput = document.getElementById("speedVal");
+speedSlider.value = jsonObj.orbitScale;
+speedOutput.innerHTML = speedSlider.value;
+
+speedSlider.oninput = function() {
+  speedOutput.innerHTML = this.value;
+  jsonObj.orbitScale = this.value;
+  jsonObj.rotationScale = this.value;
+
+}
+
+
+
+//
 /*document.getElementById("showEarth").addEventListener("click", function(){
   for (var i=0; i<jsonObj.numPlanets; i++){
     jsonObj.planets[i].beingViewed = "false";

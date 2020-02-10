45982ddb15fb88e5d84b7e9c26b68be930d9bd22
asnow4u <asnow4u@gmail.com>
2020-02-09 18:34:33 -0800

    try that

--- dist/sw.js
+++ dist/sw.js
@@ -1,2 +1,59 @@
-if(!self.define){const e=async e=>{if("require"!==e&&(e+=".js"),!c[e]&&(await new Promise(async s=>{if("document"in self){const c=document.createElement("script");c.src=e,document.head.appendChild(c),c.onload=s}else importScripts(e),s()}),!c[e]))throw new Error(`Module ${e} didn’t register its module`);return c[e]},s=async(s,c)=>{const r=await Promise.all(s.map(e));c(1===r.length?r[0]:r)};s.toUrl=e=>`./${e}`;const c={require:Promise.resolve(s)};self.define=(s,r,f)=>{c[s]||(c[s]=new Promise(async c=>{let i={};const o={uri:location.origin+s.slice(1)},l=await Promise.all(r.map(s=>"exports"===s?i:"module"===s?o:e(s))),n=f(...l);i.default||(i.default=n),c(i)}))}}define("./sw.js",["./workbox-7c85bfc1"],(function(e){"use strict";self.addEventListener("message",e=>{e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()}),e.precacheAndRoute([{url:"index.html",revision:"ed7c930ad3308c2946aaf64e29da249a"},{url:"model/planets-glb/neptune/Neptune.glb",revision:"b22fc6c5706582d982eb867be9c19159"},{url:"model/planets-glb/uranus/Uranus.glb",revision:"82af7750084d9c843eec63a5655eaced"},{url:"model/planets-glb/venus/Venus.glb",revision:"97df6b856aabf83f455d10884bf1f7f1"},{url:"polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js",revision:"cff507bc95ad1d6bf1a415cc9c8852b0"},{url:"polyfills/dynamic-import.b745cfc9384367cc18b42bbef2bbdcd9.js",revision:"ed55766050be285197b8f511eacedb62"},{url:"polyfills/webcomponents.d406f4685fdfb412c61f23b3ae18f2dc.js",revision:"b1db7cb76380495a55ff4f65a9648f0e"},{url:"solar-0943ed87.js",revision:"15fe12358eed557baa7a93d220fb624b"},{url:"solarSystem.json",revision:"cd524a4f37fd597796f682bb83021632"},{url:"src/solar.js",revision:"fef4297fce1650223a0483c665f7dffd"},{url:"style.css",revision:"a8c9c8f9cdb280da77523b371625fbf1"}],{})}));
-//# sourceMappingURL=sw.js.map
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "ed7c930ad3308c2946aaf64e29da249a"
+  },
+  {
+    "url": "polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js",
+    "revision": "cff507bc95ad1d6bf1a415cc9c8852b0"
+  },
+  {
+    "url": "polyfills/dynamic-import.b745cfc9384367cc18b42bbef2bbdcd9.js",
+    "revision": "ed55766050be285197b8f511eacedb62"
+  },
+  {
+    "url": "polyfills/webcomponents.d406f4685fdfb412c61f23b3ae18f2dc.js",
+    "revision": "b1db7cb76380495a55ff4f65a9648f0e"
+  },
+  {
+    "url": "solar-0943ed87.js",
+    "revision": "15fe12358eed557baa7a93d220fb624b"
+  },
+  {
+    "url": "src/solar.js",
+    "revision": "fef4297fce1650223a0483c665f7dffd"
+  },
+  {
+    "url": "style.css",
+    "revision": "a8c9c8f9cdb280da77523b371625fbf1"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));

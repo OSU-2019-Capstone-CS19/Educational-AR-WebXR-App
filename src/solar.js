2688fd5ad1a4a58f77654d497bacdddbc7781ef8
asnow4u <asnow4u@gmail.com>
2020-02-09 19:23:12 -0800

    commit

--- src/solar.js
+++ src/solar.js
@@ -507,7 +507,7 @@ window.addEventListener( 'click', handler, false);
 window.addEventListener( 'touchstart', handler, false);
 
 function handler(event){
-
+    console.log("EVENT HANDLER");
     mouse.x = (event.clientX / window.innerWidth) *2 -1;
     mouse.y = - (event.clientY / window.innerHeight) *2 +1;
 

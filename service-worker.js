4c7d2d84d218e87207ccd3c7fa4a7107ac5032d3
asnow4u <asnow4u@gmail.com>
2020-02-23 16:23:54 -0800

    Copied from branch 3D-Scenes

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+importScripts('/node_modules/workbox-sw/build/workbox-sw.js');
+
+if (workbox){
+  console.log('Workbox loaded');
+} else {
+  console.log('Workbox not loaded');
+}
+
+workbox.routing.registerRoute(
+  /\.css$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'cssCache',
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.js$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'jsCache',
+  })
+);
+
+workbox.routing.registerRoute(
+  // Cache assets files.
+  /\.glb$/,
+  // Use the cache if it's available.
+  new workbox.strategies.CacheFirst({
+    cacheName: 'assetCache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 15,
+        // Cache for a maximum of 3 weeks.
+        maxAgeSeconds: 21 * 24 * 60 * 60,
+      })
+    ],
+  })
+);

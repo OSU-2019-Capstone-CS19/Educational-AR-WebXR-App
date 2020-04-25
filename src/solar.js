6b42319b3f8cfbff6aec14c440a385106ac7d736
mckencod <mckencod@oregonstate.edu>
2020-04-25 14:38:12 -0700

    Added the up to date quiz info.

--- src/solar.js
+++ src/solar.js
@@ -1620,4 +1620,36 @@ function resetSolarSystem(){
   showSolarSystem = false;
 }
 
+// let xrButton = document.getElementById('xr-button');
+
+// xrButton.addEventListener('click', toggleAR);
+
+//Checking the quiz submission.
+function quizCheck() {
+  var question1 = document.quiz.question1.value;
+  var question2 = document.quiz.question2.value;
+  var question3 = document.quiz.question3.value;
+  var question4 = document.quiz.question4.value;
+  var correct = 0;
+
+  if (question1 == "Earth") {
+    correct++;
+  }
+
+  if (question2 == "Jupiter") {
+    correct++;
+  }
+
+  if (question3 == "Mercury") {
+    correct++;
+  }
+
+  if (question4 == "67") {
+    correct++;
+  }
+
+  document.getElementById("postSubmit").style.visibility = "visible";
+  document.getElementById("correctAnswers").innerHTML = "There were " + correct + " correct answers.";
+}
+
 init();
